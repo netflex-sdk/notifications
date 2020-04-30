a29e8a1f333c31f81c2dfae499a9b92e9a3e5101
Thomas Alrek <thomas@alrek.no>
2020-04-30 15:49:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Netflex Notifications Mail Driver for Laravel MailManager
+
+## Setup
+
+In `config/mail.php`:
+
+```php
+[
+  'driver' => 'notifications',
+]
+```
---
+++ src/Providers/NotifcationsServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Netflex\Notifications\Providers;
+
+use Illuminate\Mail\MailServiceProvider as ServiceProvider;
+use Netflex\Notifications\Transport\NotificationsTransport;
+
+class NotifcationsServiceProvider extends ServiceProvider
+{
+  public function boot()
+  {
+    if ($this->app->has('mail.manager')) {
+      $this->app->make('mail.manager')
+        ->extend('notifications', function () {
+          return new NotificationsTransport;
+        });
+    }
+  }
+}
