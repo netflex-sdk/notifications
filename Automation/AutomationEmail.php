ef9d0c29c561fe61b56f5996e31304d4b936789d
Thomas <thomas@alrek.no>
2022-04-26 11:23:56 +0000

    prepare release

---
+++ Automation/AutomationEmail.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Netflex\Notifications\Automation;
+
+use Exception;
+use Netflex\API\Facades\API;
+use Netflex\Query\QueryableModel;
+use Netflex\Customers\Customer;
+use Netflex\Notifications\Automation\AutomationEmailJob;
+use Netflex\Query\Exceptions\NotFoundException;
+
+class AutomationEmail extends QueryableModel
+{
+    protected $relation = 'newsletter';
+
+    public static function find($id)
+    {
+        try {
+            $response = API::get('relations/newsletters/' . $id, true);
+            $newsletter = (new static)->newFromBuilder($response);
+            $newsletter->exists = true;
+
+            if ($newsletter->automation && !$newsletter->is_template) {
+                return $newsletter;
+            }
+        } catch (Exception $e) {
+            return null;
+        }
+
+        return null;
+    }
+
+    public static function findOrFail($id)
+    {
+        if ($newsletter = static::find($id)) {
+            return $newsletter;
+        }
+
+        abort(404);
+    }
+
+    /**
+     * @param Customer|string|int $user User, email or user id
+     * @return AutomationEmailJob
+     */
+    public function to($user, ?array $payload = [])
+    {
+        $userId = $user;
+
+        if (!is_numeric($user)) {
+            if ($user instanceof Customer) {
+                $userId = $user->id;
+            } else {
+                $user = Customer::resolve($user);
+                $userId = $user->id;
+            }
+        }
+
+        if ($userId && is_numeric($userId)) {
+            return AutomationEmailJob::make($this->id, $userId, $payload);
+        }
+
+        throw new NotFoundException('User not found');
+    }
+}
---
+++ Automation/Jobs/AutomationEmailJob.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Netflex\Notifications\Automation;
+
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Netflex\API\Facades\API;
+use Netflex\Customers\Customer;
+
+class AutomationEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $newsletterId;
+    public $customerId;
+    public $payload;
+    public $locale;
+
+    protected function __construct($newsletterId)
+    {
+        $this->newsletterId = $newsletterId;
+        $this->locale = app()->getLocale();
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->locale = app()->getLocale();
+
+        return API::post('relations/newsletters/send/single/' . $this->newsletterId, [
+            'customer_id' => $this->customerId,
+            'data' => $this->payload
+        ]);
+    }
+
+    /**
+     * @param int|null $newsletterId
+     * @param int|null $customerId
+     * @param array|null $payload
+     * @return static
+     */
+    public static function make($newsletterId, $customerId = null, ?array $payload = [])
+    {
+        $automationEmail = new static($newsletterId);
+        $automationEmail->newsletterId = $newsletterId;
+        $automationEmail->customerId = $customerId;
+        $automationEmail->payload = $payload;
+        return $automationEmail;
+    }
+
+    public function send(?Carbon $sendAt = null)
+    {
+        return dispatch($this)->delay($sendAt);
+    }
+
+    public function withPayload(array $payload = [])
+    {
+        $this->payload = $payload;
+        return $this;
+    }
+
+    public function toCustomer($customer)
+    {
+        if ($customer instanceof Customer) {
+            $this->customerId = $customer->id;
+        } else {
+            $this->customerId = $customer;
+        }
+
+        return $this;
+    }
+}
