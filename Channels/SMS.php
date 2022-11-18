bc5234c27a9c761ba15ca5e85a41b3d73977b414
Thomas <thomas@alrek.no>
2022-11-18 09:07:02 +0000

    prepare release

--- Channels/SMS.php
+++ Channels/SMS.php
@@ -8,6 +8,7 @@ use Netflex\API\Facades\API;
 use Netflex\Foundation\Variable;
 
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Str;
 
 class SMS
 {
@@ -21,6 +22,21 @@ class SMS
             $from = method_exists($notification, 'from') ? $notification->from() : Variable::get('sms_from');
             $to = ($notifiable->phone_countrycode ? ('+' . $notifiable->phone_countrycode) : null) . $notifiable->phone;
             $message = $notification->toSMS($notifiable);
+
+            if (method_exists($notifiable, 'resolvePhoneNumber')) {
+                $resolvedPhoneNumber = $notifiable->resolvePhoneNumber($notifiable);
+                if ($resolvedPhoneNumber && array_key_exists('countrycode', $resolvedPhoneNumber) && array_key_exists('phone', $resolvedPhoneNumber)) {
+                    $countrycode = $resolvedPhoneNumber['countrycode'];
+                    if (!Str::startsWith($countrycode, '+')) {
+                        $countrycode = '+' . $countrycode;
+                    }
+                    $phone = $resolvedPhoneNumber['phone'];
+
+                    if ($countrycode && $phone) {
+                        $to = $countrycode . $resolvedPhoneNumber['phone'];
+                    }
+                }
+            }
 
             API::post('relations/sms/send/single', [
                 'to' => [$to],
